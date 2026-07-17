9c95c9e332449593ed07daf072a8828aca2de5a7
babblebey <babblebey@gmail.com>
2026-07-17 03:20:04 +0100

    feat(cli): add version command

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -3,19 +3,37 @@
 
 const { createCore } = require('@demonorepo-pnpm/core');
 const { formatMessage } = require('@demonorepo-pnpm/utils');
+const { version } = require('./package.json');
 
 const [, , command, ...args] = process.argv;
 
 const core = createCore({ name: args[0] || 'world' });
 
+function printUsage() {
+  console.log('Usage: demonorepo <command> [args...]');
+  console.log('');
+  console.log('Commands:');
+  console.log('  greet [name]         Print a greeting');
+  console.log('  log [message...]     Print a formatted log message');
+  console.log('  version              Show CLI version');
+}
+
 switch (command) {
+  case 'version':
+  case '-v':
+  case '--version':
+    console.log(version);
+    break;
   case 'greet':
     console.log(core.greet());
     break;
   case 'log':
     core.log(args.join(' ') || 'Hello from CLI!');
     break;
+  case undefined:
+    printUsage();
+    break;
   default:
     console.log(formatMessage('cli', `Unknown command: ${command || '(none)'}`));
-    console.log('Usage: demonorepo <greet|log> [args...]');
+    printUsage();
 }
